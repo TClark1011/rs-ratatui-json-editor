8c859bdf5593bb864a0a7b976d324de6026b30ed
Thomas Clark <thomas.clark@bigtincan.com>
2024-11-14 13:05:48 +1100

    create README with auto generated documentation

---
+++ README.md
@@ -0,0 +1,16 @@
+# Project Name Here
+
+<!-- HELP_OUTPUT_START -->
+
+```
+Usage: rs-ratatui-json-editor [OPTIONS] [INPUT_FILE]
+
+Arguments:
+  [INPUT_FILE]  The input file to read from
+
+Options:
+      --dry   Whether to run in "dry" mode (no changes will be written to the output file)
+  -h, --help  Print help
+```
+
+<!-- HELP_OUTPUT_END -->
---
+++ scripts/readme-help.sh
@@ -0,0 +1,21 @@
+# we embed the app's `--help` output into the README as
+# documentation
+
+cargo run -- --help > help_output.tmp
+
+# echo "$HELP_OUTPUT"
+
+# Define start and end markers in README
+START_MARKER="<!-- HELP_OUTPUT_START -->"
+END_MARKER="<!-- HELP_OUTPUT_END -->"
+
+# Update the README file in place
+awk -v start_marker="$START_MARKER" -v end_marker="$END_MARKER" '
+    BEGIN { in_section=0 }
+    $0 ~ start_marker { print; print ""; print "```"; while ((getline < "help_output.tmp") > 0) print; print "```"; print ""; in_section=1; next }
+    $0 ~ end_marker { in_section=0 }
+    !in_section { print }
+' README.md > README.tmp && mv README.tmp README.md
+
+# Remove the temporary file
+rm help_output.tmp
